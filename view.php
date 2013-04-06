80b921fe2a1e18f05e04f663084b4da378764c5b
metaplinius <antonius@b-frie.de>
2013-04-06 12:02:19 +0200

    added messagebox on user view page

--- view.php
+++ view.php
@@ -15,6 +15,15 @@ function view ($uid) {
 					<h1>".$data['name']."</h1>
 					<h2>Beschreibung</h2>
 					<p>".$data['desp']."</p>
+					<div id=\"msgfwd\">
+						<h2>Nachricht schreiben</h2>
+						<form action=\"newpost.php\" method=\"POST\">
+							<textarea class=\"msgtext\" name=\"content\"></textarea>
+							<input type=\"submit\" class=\"msgsubmit\" value=\"Senden\">
+							<input type=\"hidden\" name=\"user\" value=\"".$GLOBALS["uid"]."\">
+							<input type=\"hidden\" name=\"dest\" value=\"".$data['name']."\">
+						</form>
+					</div>
 				</div>
 			</div>";
 }
