d14c9f3ac6b5e14fd697ee9a6cd033e5c35d1595
metaplinius <antonius@b-frie.de>
2013-04-07 20:29:48 +0200

    switched addknown.php to secure prepared statements

--- addknown.php
+++ addknown.php
@@ -1,17 +1,24 @@
 <?php
+//SQL-Injection safe
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include("logincheck.php");
 include("database.php");
 include("getuid.php");
-$sql = "SELECT * FROM uurel WHERE uida=".$uid." AND uidp=".$_GET['uid'].";";
-$res = $db->query($sql);
+$sql = "SELECT * FROM uurel WHERE (uida=".$uid." AND uidp=?);";
+$stmt = $db->prepare($sql);
+$stmt->bind_param('s',$_GET['uid']);
+$stmt->execute();
+$stmt->store_result();
 if ($uid==$_GET['uid']) {
 	header("Location: main.php");
 }
-elseif ( !($res->num_rows) ) {
-	$sql = "INSERT INTO uurel SET uida=".$uid.", uidp=".$_GET['uid'].";";
-	$res = $db->query($sql);
+elseif ( !($stmt->num_rows) ) {
+	$stmt->close();
+	$sql = "INSERT INTO uurel SET uida=".$uid.", uidp=?;";
+	$stmt = $db->prepare($sql);
+	$stmt->bind_param('s',$_GET['uid']);
+	$stmt->execute();
 	header("Location: main.php?msg=Gemerkt");
 }
 else {
