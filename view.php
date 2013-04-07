8b8d6f418cd0403abd8ca87bd927832014de37c3
metaplinius <antonius@b-frie.de>
2013-04-07 18:23:48 +0200

    beginning to make things secure (using php prepared statements)

--- view.php
+++ view.php
@@ -1,9 +1,15 @@
 <?php
+//SQL-Injection safe
 function view ($uid) {
 	$db = $GLOBALS['db'];
-	$sql = "SELECT name, desp FROM user WHERE uid=".$uid.";";
-	$res = $db->query($sql);
-	$data = $res->fetch_assoc();
+	$sql = "SELECT name, desp FROM user WHERE uid=?;";
+	$stmt = $db->prepare($sql);
+	$stmt->bind_param('i',$uid);
+	$stmt->execute();
+	$stmt->bind_result($name,$desp);
+	$stmt->fetch();
+	/*$res = $db->query($sql);
+	$data = $res->fetch_assoc();*/
 	echo "
 			<div id=\"main\">
 				<div class=\"centerbox\">
@@ -12,16 +18,16 @@ function view ($uid) {
 							<img src=\"img/close.png\">
 						</a>
 					</div>
-					<h1>".$data['name']."</h1>
+					<h1>".$name."</h1>
 					<h2>Beschreibung</h2>
-					<p>".$data['desp']."</p>
+					<p>".$desp."</p>
 					<div id=\"msgfwd\">
 						<h2>Nachricht schreiben</h2>
 						<form action=\"newpost.php\" method=\"POST\">
 							<textarea class=\"msgtext\" name=\"content\"></textarea>
 							<input type=\"submit\" class=\"msgsubmit\" value=\"Senden\">
 							<input type=\"hidden\" name=\"user\" value=\"".$GLOBALS["uid"]."\">
-							<input type=\"hidden\" name=\"dest\" value=\"".$data['name']."\">
+							<input type=\"hidden\" name=\"dest\" value=\"".$name."\">
 						</form>
 					</div>
 				</div>
