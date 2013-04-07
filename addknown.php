51b411885e4884e3f283555778319725df9793f6
metaplinius <antonius@b-frie.de>
2013-04-07 19:34:53 +0200

    addknown.php improved -- you cant add yourself

--- addknown.php
+++ addknown.php
@@ -6,7 +6,10 @@ include("database.php");
 include("getuid.php");
 $sql = "SELECT * FROM uurel WHERE uida=".$uid." AND uidp=".$_GET['uid'].";";
 $res = $db->query($sql);
-if ( !($res->num_rows) ) {
+if ($uid==$_GET['uid']) {
+	header("Location: main.php");
+}
+elseif ( !($res->num_rows) ) {
 	$sql = "INSERT INTO uurel SET uida=".$uid.", uidp=".$_GET['uid'].";";
 	$res = $db->query($sql);
 	header("Location: main.php?msg=Gemerkt");
